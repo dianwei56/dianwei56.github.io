51b19920d50acda27f5e41318c12ac8e1f49bf01
wudianwei <47598134+wudianwei@users.noreply.github.com>
2019-02-13 20:41:56 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,6 @@
-var test = ['test1','test2','test3']
+var test = ['test1','test2','test3','test4']
+var str = "";
 for(var i = 0; i < test.length; i++){
-    console.log(test[i]);
+    str += test[i];
 }
+console.log(str);
